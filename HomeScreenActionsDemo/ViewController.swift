e3bbe426fc0948e66526afa0994194761735a356
MostafaAbdElFatah <m.abdelfatah@gmail.com>
2020-04-18 15:43:47 +0200

    Initial Commit

---
+++ HomeScreenActionsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeScreenActionsDemo
+//
+//  Created by Mostafa Abd ElFatah on 4/18/20.
+//  Copyright © 2020 Mostafa Abd ElFatah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
