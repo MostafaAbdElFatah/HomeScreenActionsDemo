810120263233885d3466a53ae5edd9fbac5df791
MostafaAbdElFatah <m.abdelfatah@gmail.com>
2020-04-18 16:06:03 +0200

    first commit

--- HomeScreenActionsDemo/AppDelegate.swift
+++ HomeScreenActionsDemo/AppDelegate.swift
@@ -12,14 +12,18 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+//        if (UIApplication.shared.shortcutItems?.filter({ $0.type == "com.app.myshortcut" }).first == nil) {
+//            UIApplication.shared.shortcutItems?.insert(UIMutableApplicationShortcutItem(type: "com.app.myshortcut", localizedTitle: "Shortcut Title"), at: 0)
+//            UIApplication.shared.shortcutItems?.append(UIMutableApplicationShortcutItem(type: "com.app.myshortcut", localizedTitle: "Shortcut Title"))
+//        }
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
